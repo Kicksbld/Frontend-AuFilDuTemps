8df33e577377f7419ee0a6f759e4f306d3b183c7
Celeste-clt <celeste.colletti@icloud.com>
2025-02-26 10:50:48 +0100

    import flowbite

--- src/views/ProductsVue.vue
+++ src/views/ProductsVue.vue
@@ -2,10 +2,20 @@
     <main class="grid place-content-center min-h-[50vh] w-full">
         <h1 class="text-4xl text-red-500">Products Page under development</h1>
     </main>
+
+    <carousel :pictures="pictures" />
 </template>
+
 
 <script>
   
+import { Carousel } from 'flowbite-vue'
+
+const pictures = [
+  {src: './img/carous1', alt: 'img 1'},
+  {src: '/images/img-2.svg', alt: 'Image 2'},
+  {src: '/images/img-3.svg', alt: 'Image 3'},
+]
 </script>
 
 <style lang="css" scoped>
