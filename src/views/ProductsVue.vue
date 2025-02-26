e0c901ea2bf9e6f667fa2413aac7db87ef1c8824
Celeste-clt <celeste.colletti@icloud.com>
2025-02-26 15:27:50 +0100

    add img carous

--- src/views/ProductsVue.vue
+++ src/views/ProductsVue.vue
@@ -2,19 +2,16 @@
     <main class="grid place-content-center min-h-[50vh] w-full">
         <h1 class="text-4xl text-red-500">Products Page under development</h1>
     </main>
-
-    <carousel :pictures="pictures" />
+  <fwb-carousel :pictures="pictures" />
 </template>
 
-
-<script>
-  
-import { Carousel } from 'flowbite-vue'
+<script setup>
+import { FwbCarousel } from 'flowbite-vue'
 
 const pictures = [
-  {src: './img/carous1', alt: 'img 1'},
-  {src: '/images/img-2.svg', alt: 'Image 2'},
-  {src: '/images/img-3.svg', alt: 'Image 3'},
+  {src: './img/carous1.png', alt: 'Image 1'},
+  {src: './img/carous2.png', alt: 'Image 2'},
+  {src: './img/carous3.png', alt: 'Image 3'},
 ]
 </script>
 
