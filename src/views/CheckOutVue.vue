2b8d47b115855840e12a0653634df7510bda1fe0
Killian <killian.boularand@outlook.fr>
2025-03-01 18:54:50 +0100

    payement feature added

--- src/views/CheckOutVue.vue
+++ src/views/CheckOutVue.vue
@@ -1,17 +1,53 @@
 <template>
     <main class="grid place-content-center min-h-[50vh] w-full">
-        <h1 class="text-4xl text-red-500">checkout Page under development</h1>
-        <div class="flex flex-col gap-4 mt-4">
-            <RouterLink class="text-blue-500" to="/cart">Cart</RouterLink>
-            <RouterLink class="text-blue-500" to="/order-review">Order Review</RouterLink>
-        </div>
-    </main>
-</template>
-
-<script>
+      <h1 class="text-4xl text-red-500">Checkout Page under development</h1>
+      <div class="flex flex-col gap-4 mt-4">
+        <RouterLink class="text-blue-500" to="/cart">Cart</RouterLink>
+        <RouterLink class="text-blue-500" to="/order-review"
+          >Order Review</RouterLink
+        >
   
-</script>
-
-<style scoped>
-
-</style>
+        <button
+          class="px-6 py-2 border-none bg-blue-500 text-white rounded-md cursor-pointer"
+          @click="handleCheckout"
+        >
+          Passer Au Payement
+        </button>
+      </div>
+    </main>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        productId: "price_1QxqwxBYXgW8ATru1sFnAEuu", 
+        BACKEND_URL: import.meta.env.VITE_BACKEND_URL
+      };
+    },
+    methods: {
+      async handleCheckout() {
+        try {
+          const response = await fetch(`${this.BACKEND_URL}/payement`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              priceId: this.productId, 
+            }),
+          });
+  
+          const data = await response.json();
+          if (data.url) {
+            window.location.href = data.url; 
+          }
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped></style>
