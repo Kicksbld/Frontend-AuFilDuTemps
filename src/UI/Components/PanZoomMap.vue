92b36eb6a29c3d04da01f76ef0d1b38ffd973a33
Killian <killian.boularand@outlook.fr>
2025-03-09 15:56:21 +0100

    :sparkles: test vue-panzoom library for competition game dev

---
+++ src/UI/Components/PanZoomMap.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="map-container">
+      <SvgPanZoom
+        class="svg-pan"
+        :zoomEnabled="true"
+        :controlIconsEnabled="true"
+        :fit="false"
+        :center="true"
+        @created="registerSvgPanZoom"
+      >
+        <svg class="w-full h-full" viewBox="0 0 2000 2000">
+       
+          <image x="0" y="0" width="2000" height="2000" xlink:href="/src/assets/img/png/vue.svg" />
+        
+          <circle cx="500" cy="700" r="50" fill="red" @click="onCharacterFound" />
+        </svg>
+      </SvgPanZoom>
+    </div>
+  </template>
+  
+  <script>
+  import { SvgPanZoom } from "vue-svg-pan-zoom"; // ✅ Correction de l'import
+  
+  export default {
+    components: { SvgPanZoom },
+    data() {
+      return {
+        svgpanzoom: null, 
+      };
+    },
+    methods: {
+      registerSvgPanZoom(instance) {
+        this.svgpanzoom = instance; // Stocke l'instance pour contrôler le zoom plus tard
+      },
+      onCharacterFound() {
+        alert("Bravo ! Vous avez trouvé le personnage 🎉");
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .map-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background-color: #f5f5f5;
+  }
+  
+  .svg-pan {
+    width: 100%;
+    height: 100%;
+    border: 1px solid black;
+  }
+  </style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { useDataBaseStore } from "../stores/dataBaseData";
 import { onMounted, computed } from "vue";
 import { RouterLink } from "vue-router";
+import PanZoomMap from "../UI/Components/PanZoomMap.vue";
 
 const { fetchDBData, state } = useDataBaseStore();
 
@@ -20,6 +21,7 @@ const users = computed(() => Array.isArray(state.data) ? state.data : []);
       <RouterLink class="text-blue-500" to="/collections">Collections</RouterLink>
       <RouterLink class="text-blue-500" to="/competition-game">Competition Game</RouterLink>
     </div>
+    <PanZoomMap />
   </main>
 
 </template>
