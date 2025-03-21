7c023d5c4d50302989dd136809f82578f8bdeb41
Killian <killian.boularand@outlook.fr>
2025-03-22 00:14:34 +0100

    Header fixed

--- src/UI/Components/Header.vue
+++ src/UI/Components/Header.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="w-full min-h-[100vh] flex items-end">
-        <img src="/src/assets/img/png/bg-header.png" class="absolute top-0 left-o w-screen -z-20" alt="video-header" />
-        <div class="bg-header-box max-w-[1036px] space-y-[15px] px-[38px] py-[32px] mr-[6.198vw]  ml-[6.198vw] mb-[3.75vw]">
+        <img src="/src/assets/img/png/bg-header.png" class="absolute top-0 left-o w-screen h-full object-cover -z-20" alt="video-header" />
+        <div class="bg-header-box max-w-[1036px] space-y-[15px] px-[38px] py-[32px] mx-[6.198vw] mb-[3.75vw]">
             <Typography font="scholar" variant="h1" theme="headerText">
                 Tisser des souvenirs pour ne pas laisser filer le temps
             </Typography>
             <Typography font="halenoir" variant="h3" theme="headerText">
-                Les souvenirs se forment avec le temps et ces derniers sont plus marquant lorsqu’ils touchent notre âme.
-                C’est à travers l’art que l’héritage des souvenirs se fait et c’est ce que l’on cherche à partager.
-                Grâce à la mode , on partage cette culture de l’art
+                Les souvenirs se forment avec le temps et ces derniers sont plus marquant lorsqu'ils touchent notre âme.
+                C'est à travers l'art que l'héritage des souvenirs se fait et c'est ce que l'on cherche à partager.
+                Grâce à la mode , on partage cette culture de l'art
             </Typography>
         </div>
     </div>
