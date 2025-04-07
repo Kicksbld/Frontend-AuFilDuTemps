60d412c0704262e812233adef67fe7a9008839c6
Killian <killian.boularand@outlook.fr>
2025-04-07 14:16:42 +0200

    issues fixed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/vue"
+
 export const authClient = createAuthClient({
-    /** the base url of the server (optional if you're using the same domain) */
-    baseURL: process.env.VITE_BACKEND_URL,
+    baseURL: import.meta.env.VITE_BACKEND_URL,
 })
 
