d60a9a3e1299d6f958f028f2f2b15e82432a8304
Killian <killian.boularand@outlook.fr>
2025-05-20 14:59:14 +0200

    :rocket: account pages logic done

--- src/services/index.js
+++ src/services/index.js
@@ -6,6 +6,6 @@ export const orders    = makeService('orders')
 export const products    = makeService('products')
 export const users    = makeService('users')
 export const addresses    = makeService('addresses')
-export const favourites    = makeService('favourites')
+export const favorites    = makeService('favorites')
 export const preferences    = makeService('preferences')
 
